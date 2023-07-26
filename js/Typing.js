cc4a3fb9cf14bcad79b7843c6d1640252eb73910
imryan1919 <140376136+imryan1919@users.noreply.github.com>
2023-07-26 13:41:14 +0700

    Add files via upload

---
+++ js/Typing.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+var typed = new Typed('.element', {
+ strings: ["According to Sinnoh legend"],
+ typeSpeed:50,
+ backSpeed:50,
+ loop:true,
+        		});
+
+$('.counter').countUp();
---
+++ js/tab.js
@@ -0,0 +1,16 @@
+function openCity2(evt, cityName) {
+  var i, tabcontent2, tablinks2;
+  tabcontent = document.getElementsByClassName("tabcontent2");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks2");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen2").click();
